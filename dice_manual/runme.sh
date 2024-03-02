eaa01a09f69575ead1226db1d56f951d6cc86be7
Kyle Champlin <kyle.champlin@observeinc.com>
2024-03-01 22:01:08 -0800

    final testing

--- dice_manual/runme.sh
+++ dice_manual/runme.sh
@@ -1,9 +1,9 @@
 # Export variables for observe stuff
 # put your 12 digit tenant id here
-export TENANT_ID=""
+export TENANT_ID="166467280995"
 # put your Datastream TOKEN you created in your observe tenant here
 # be sure to create the token under the OTEL app namespace
-export DATASTREAM_TOKEN=""
+export DATASTREAM_TOKEN="ds17KnCz1rkt8Bwotiva:ojUrrmdh7yrDO-Yjx97J0rNEeIDNV85u"
 
 
 
@@ -16,7 +16,7 @@ export OTEL_TRACES_EXPORTER="otlp"
 export OTEL_EXPORTER_OTLP_TRACES_ENDPOINT="https://${TENANT_ID}.collect.observeinc.com/v1/otel/v1/traces"
 export OTEL_EXPORTER_OTLP_HEADERS="Authorization=Bearer ${DATASTREAM_TOKEN}"
 export OTEL_NODE_RESOURCE_DETECTORS=env,host
-export OTEL_SERVICE_NAME="example-node"
+export OTEL_SERVICE_NAME="example-node-manual-raw"
 export NODE_OPTIONS="--require @opentelemetry/auto-instrumentations-node/register"
 
 # Run app
