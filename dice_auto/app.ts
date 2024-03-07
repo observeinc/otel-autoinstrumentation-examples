06fc49e2ab56ddb080f69ef6b553c5077c7f35c8
Kyle Champlin <kyle.champlin@observeinc.com>
2024-03-06 22:05:16 -0800

    prep for initial release

--- dice_auto/app.ts
+++ dice_auto/app.ts
@@ -32,8 +32,7 @@ app.post('/postroll',(req,res)=>{
 
 app.get('/ohno', (req, res) => {
  //modify the url in any way you want
-  var urls = ['http://google.com/','http://datadicks.com','http://unknown.observeinc.com'];
-  //var urls = ['http://datadicks.com']
+ var urls = ['http://google.com/','https://astronomy.sandbox.sockshop.biz/api/products','http://unknown.observeinc.com'];
   request(urls[getRandomInt(urls.length)]).pipe(res);
 
 });
